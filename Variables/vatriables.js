dea49ed099df8f1d8d955f7efe51f2bd728a87f7
QA-Automation-2026 <veerababumsg@gmail.com>
2026-07-05 21:43:38 +0530

    variables demo

---
+++ Variables/vatriables.js
@@ -0,0 +1,5 @@
+"use strict";
+var number = 123456;
+let l = 546;
+let total = number + l;
+console.log(total);
---
+++ Variables/vatriables.ts
@@ -0,0 +1,20 @@
+var number : number = 123456
+let l : number = 546
+const c : string = "total value"
+let total : number = number+l
+console.log(total)
+
+var i = c+"  "+total
+console.log(i)
+
+var names : string = "veerababu"
+
+var dashboard :string = `Welcome ${names}`
+
+console.log(dashboard)
+
+let firstName: string = "Veeru";
+let message: string = `Welcome ${firstName}`;
+
+console.log(message)
+
