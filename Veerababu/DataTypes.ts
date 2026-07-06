623280e154e6456455b7e1014515f3d449134964
QA-Automation-2026 <veerababumsg@gmail.com>
2026-07-06 22:29:56 +0530

    Primitive Data Types Demo

---
+++ Veerababu/DataTypes.ts
@@ -0,0 +1,64 @@
+
+
+// typeOfVariable variableName :(colon) datatype = value
+
+// String datatype
+
+let stringDemo : string = "Playwright Session";
+
+let singleQuote : string = 'Playwright';
+
+let check : string = ` Welcome ${singleQuote} automation  ${stringDemo}`;
+
+console.log(stringDemo)
+
+console.log(singleQuote)
+
+console.log(check)
+
+// numbers
+
+let si : number = 5000
+si = 8000
+console.log(si)
+
+
+var age :number = 5588888888888888888888888888888888888888888888888888.558
+
+const voterIdEligibility : number = 18
+
+let isDisplayed : boolean = true
+
+let isEnabled : boolean = false
+
+console.log(!isEnabled)
+
+// null data type
+
+let productValue : null = null
+
+
+console.log(dec)
+
+var dec : string = "value type"
+
+
+// big int
+
+
+let bigNumbersType : bigint = 123456888888n
+
+console.log(bigNumbersType)
+
+//25.56 = 26
+//25.547 = 25.55
+
+let name1 : symbol =  Symbol("satish") // 
+
+let name2 : symbol =  Symbol("satish")
+
+console.log(name1== name2)
+
+
+
+
