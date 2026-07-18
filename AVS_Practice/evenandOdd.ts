d5ded61d61dded96ecd055e12f03f26099a44946
aniladavi <anil.adavi@gmail.com>
2026-07-18 09:19:20 +0530

    Fibanaci, evenodd

---
+++ AVS_Practice/evenandOdd.ts
@@ -0,0 +1,18 @@
+let series1 : number = 20
+
+for (let j : number=1;j<=series1;j++)
+{
+    if(j==1)
+    {
+     console.log("Prime number " + j)
+ 
+    } else if (j % 2 == 0)
+    {
+        console.log("Even number " + j)
+    }
+    else
+    {
+        console.log("Odd number " + j)
+
+    }
+}
