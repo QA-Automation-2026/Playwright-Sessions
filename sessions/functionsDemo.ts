65413716ecea8f41de138eaa584491eb605f17b4
QA-Automation-2026 <veerababumsg@gmail.com>
2026-07-21 22:19:22 +0530

    functions demo

---
+++ sessions/functionsDemo.ts
@@ -0,0 +1,33 @@
+class demo{
+
+    // Syntax
+    // function name(params:type) : return type{
+        
+    // }
+    
+    // Zero parameters with no return type
+    // Parameters with return type
+
+    addition(a:number, b :string) : boolean{
+
+        console.log(`Addition of two numbers ${b}`)
+        let c = "return some statement"
+        return false
+        }
+    subtraction(){
+
+    }
+    multiplication(){
+
+    }
+    
+    
+
+}
+const obj = new demo();
+let total = obj.addition(15,'25');
+console.log("Total is : "+total)
+
+
+// add numbers from 1 to 10 and no return statement
+// add list of names and return total name
